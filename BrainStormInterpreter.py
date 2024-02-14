6290702e5839c1a11bdab3f39eff196dcd9411b9
ArthurSpeziali <arthur.speziali2022@gmail.com>
2024-02-14 11:00:52 -0300

    Interpretador de BrainFuck
    Terminei com o interpretador de Brainfuck inteiro.

--- BrainStormInterpreter.py
+++ BrainStormInterpreter.py
@@ -2,18 +2,19 @@
 
 from modules.Exceptions import Exceptions
 
-
 with open('script.bstm') as script_file:
     script = script_file.read().replace('\n', '').replace(' ', '')
 
 Exceptions.script = script
 array_bit = [0]
 home = 0
-lenarray = 4096
-for enum, char in enumerate(script):
+item = 0
+while item < len(script):
+    char = script[item]
+    
     if char == '>':
-        if lenarray == 4096:
-            raise(Exceptions(enum, 4096).OutofRange)
+        if len(array_bit) == 4096:
+            raise(Exceptions(item, 4096).OutofRange)
         
         if home == len(array_bit) -1:
             array_bit.append(0)
@@ -23,7 +24,7 @@ for enum, char in enumerate(script):
         
     elif char == '<':
         if home == 0:
-            raise(Exceptions(enum, -1).OutofRange)
+            raise(Exceptions(item, -1).OutofRange)
         
         home -= 1
         
@@ -47,6 +48,42 @@ for enum, char in enumerate(script):
     elif char == '.':
         print(chr(array_bit[home]), end='')
         
+        
+    elif char == ',':
+        char_bit = input()
+        
+        if len(char_bit) > 1:
+            raise(Exceptions(item, len(char_bit)).MultipleCharByte)
+        
+    
+        if ord(char_bit) > 126:
+            raise(Exceptions(item, ord(char_bit)).NoAsciiChar)
+            
+        array_bit[home] = ord(char_bit)
 
-
+    
+    elif char == '[':
+        if not ']' in script[item:]:
+            raise(Exceptions(item, 0).NotOpenorCloseBrackets)
+        
+        if '[' in script[item + 1: script.find(']')]:
+            raise(Exceptions(item).DoubleLoop)
+        
+        if not '-' in script[item: script.find(']', item)]:
+            raise(Exceptions(item).InfinityLoop)
+        
+        if array_bit[home] == 0:
+            item = script.find(']', item)            
+        
+        
+    elif char == ']':
+        if not '[' in script[: item]:
+            raise(Exceptions(item, 1).NotOpenorCloseBrackets)
+            
+        if array_bit[home] > 0:
+            item = script[:item].rfind('[')
+        
+        
+    item += 1
+    
 print()     
--- modules/Exceptions.py
+++ modules/Exceptions.py
@@ -2,13 +2,21 @@ class Exceptions:
     def __init__(self, index, code=0):
         line = self.ExceptionMaker(index)
         
-        self.OutofRange = Exception(f'Array Out of Range[{code}]{line}')
+        self.OutofRange = Exception(f'Byte out of range[{code}]{line}')
+        self.MultipleCharByte = Exception(f'Multiple characters in one byte[{code}]{line}')
+        self.NoAsciiChar = Exception(f'Character is not in ascii table[{code}]{line}')
+        self.NotOpenorCloseBrackets = Exception(f"Bracket's were never opened or closed[{code}]{line}")
+        self.DoubleLoop = Exception(f'Loop within another loop[{code}]{line}')
+        self.InfinityLoop = Exception(f'Loop that never ends[{code}]{line}')
+        
     def ExceptionMaker(self, index):
         line = '\n\n'
+        line += f'Character: \033[33;1m{index + 1}\033[m'
+        line += '\n'
         
-        line += self.script[index - 5: index]
+        line += '\033[32m' + self.script[index - 5: index] + '\033[m'
         line += f'\033[31m{self.script[index]}\033[m'
-        line += self.script[index + 1: index + 6]
+        line += '\033[32m' + self.script[index + 1: index + 6] + '\033[m'
         
         line += '\n'
         line += 5 * ' ' + '↑'
